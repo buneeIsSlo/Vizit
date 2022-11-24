78e5061b00b9e9d89fa750ea205f101f38ba78fd
bunee <buneeisslo@gmail.com>
2022-11-24 22:36:23 +0530

    Feat: Reset/Clear grid functionality

--- src/js/app.js
+++ src/js/app.js
@@ -5,9 +5,14 @@ console.log("ohaider");
 
 const appGrid = grid();
 const visualizeBtn = document.querySelector(".visualize");
+const clearGridBtn = document.querySelector(".clear-grid");
 
 appGrid.renderGrid();
 window.addEventListener("resize", () => appGrid.renderGrid());
+
+clearGridBtn.addEventListener("click", () => {
+    appGrid.renderGrid();
+});
 
 visualizeBtn.addEventListener("click", () => {
     console.log("vizzing");
